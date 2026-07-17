a98fdf67ddba0a6893ec0dd4c15ad256ac18f5e9
Xinyao <hi@xinyao.me>
2026-07-17 21:41:47 +0800

    Remove tooltip arrows

--- src/renderer/src/components/ui/tooltip.tsx
+++ src/renderer/src/components/ui/tooltip.tsx
@@ -54,7 +54,7 @@ function TooltipContent({
           {...props}
         >
           {children}
-          <TooltipPrimitive.Arrow className="size-2.5 translate-y-[calc(-50%_-_2px)] rotate-45 rounded-[2px] bg-foreground fill-foreground" />
+          {/* Why: product tooltips intentionally use an arrowless surface across the app. */}
         </TooltipPrimitive.Popup>
       </TooltipPrimitive.Positioner>
     </TooltipPrimitive.Portal>
